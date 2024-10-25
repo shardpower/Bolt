225600862bf8650a387d3eda0c47a85ae17a0d0c
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2024-10-24 22:17:49 -0400

    use c.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,62 +1,31 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
-async function init() {
-    try {
-        const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
-        let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+var urlToEncode;
 
-        if (await connection.getTransport() !== "/epoxy/index.mjs") {
-            await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-            console.log("Using websocket transport");
-        }
-    } catch (error) {
-        console.error("Error setting up BareMux transport:", error);
-    }
+async function encodeUrl() {
+    await chemical.setTransport("epoxy");
+    var url = await chemical.encode(urlToEncode, {
+        service: localStorage.getItem("proxy"),
+        autoHttps: true,
+        searchEngine: "https://www.google.com/search?q=%s"
+    })
+    console.log(url)
+    localStorage.setItem("url", url);
+    window.location.href = "/browser.html";
 }
-init();
 
 
 if (form && input) {
-    Main();
-}
+    console.log("Ready to search!");
 
-function Main() {
     form.addEventListener("submit", async (event) => {
-        function isUrl(val = "") {
-            if (
-                /^http(s?):\/\//.test(val) ||
-                (val.includes(".") && val.substr(0, 1) !== " ")
-            ) {
-                return true;
-            }
-            return false;
-        }
-
         event.preventDefault();
-
-        try {
-            await registerSW();
-
-            console.log("Registering service worker...");
-        } catch (err) {
-            err.textContent = err.toString();
-            throw err;
-        }
-        var url = input.value;
-
-        if (!isUrl(url)) {
-            url = "https://www.google.com/search?q=" + url;
-        } else if (!(url.startsWith("https://") || url.startsWith("http://"))) {
-            url = `https://${url}`;
-        }
-
-
-
-        url = __uv$config.encodeUrl(url);
-
-        localStorage.setItem("url", url);
-        localStorage.setItem("loaded", false);
-        window.location.href = "/browser.html";
-    });
+        urlToEncode = input.value;
+        encodeUrl();
+    })
 }
+
+
+
+
--- public/assets/js/tabs.js
+++
@@ -1,9 +0,0 @@
-var ftabtxt = document.getElementById("ftabtxt");
-var uvframe = document.getElementById("uvFrame");
-
-if (ftabtxt && uvframe) {
-    uvframe.onload = function () {
-        ftabtxt.textContent = uvframe.contentWindow.document.title;
-    }
-
-}
