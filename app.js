681843f819ad671a474b6754821bb5d3df342917
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2024-10-28 18:29:29 -0400

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { ChemicalServer } from "chemicaljs";
 import express from "express";
+import cors from "cors";
 
 const [app, listen] = new ChemicalServer();
 const port = process.env.PORT || 8080;
@@ -8,6 +9,12 @@ const port = process.env.PORT || 8080;
 app.use(express.static("public", {
     index: "index.html",
     extensions: ["html"]
+}));
+
+app.use(cors({
+    origin: '*',
+    methods: 'GET',
+    allowedHeaders: 'Content-Type',
 }));
 
 app.serveChemical();
